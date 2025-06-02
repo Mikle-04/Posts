39275acd2b25b68ad1c0a7b55ce7544c322ed1ed
Mikle-04 <godmordor@gmail.com>
2025-06-02 23:53:28 +0300

    ReleaseDomain

---
+++ app/src/main/java/com/example/posts/domain/usecase/GetFavoritePostsUseCase.kt
@@ -0,0 +1,12 @@
+package com.example.posts.domain.usecase
+
+import com.example.posts.data.db.PostEntity
+import com.example.posts.data.repository.PostRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetFavoritePostsUseCase @Inject constructor(
+    private val repository: PostRepository
+) {
+    operator fun invoke(): Flow<List<PostEntity>> = repository.getFavoritePosts()
+}
